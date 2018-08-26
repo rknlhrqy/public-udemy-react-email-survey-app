90ee9a475d944dfb1f75b432eaa8427f7064b7c1
Kening Ren <rknlh.training@gmail.com>
2018-08-26 19:52:50 -0400

    clean up the code

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -25,7 +25,7 @@ class Header extends Component {
         <div className="nav-wrapper">
           <Link
             to={this.props.auth ? '/surveys' : '/'}
-            className="left brand-logo" href="/"
+            className="left brand-logo"
           >Emaily</Link>
           <ul className="right">
             {this.renderContent()}
